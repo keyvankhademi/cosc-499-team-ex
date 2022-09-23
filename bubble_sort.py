0b42f93edbcaef249f1d4b38591fe4dda74af192
Quinn Marshall <77088280+QuinnMarshal@users.noreply.github.com>
2022-09-23 09:51:09 -0700

    add buble sort (#12)
    * add buble sort
    
    * edited bubble sort alg

---
+++ bubble_sort.py
@@ -0,0 +1,12 @@
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        swapped = False
+        for j in range(0, len(arr) - i - 1):
+            if(arr[j]>arr[j+1]):
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+                swapped = True
+                
+        if not swapped:
+            break  
